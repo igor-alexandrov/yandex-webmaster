0a100cb45fd9f8ad2a9a60a8d0fc53fce9015af7
Igor Alexandrov <igor.alexandrov@gmail.com>
2013-05-13 01:22:30 +0400

    Updated version

--- lib/yandex-webmaster/version.rb
+++ lib/yandex-webmaster/version.rb
@@ -4,8 +4,8 @@ module Yandex
   module Webmaster
     module Version
       MAJOR = 0
-      MINOR = 1
-      PATCH = 1
+      MINOR = 2
+      PATCH = 0
       BUILD = nil
 
       STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
